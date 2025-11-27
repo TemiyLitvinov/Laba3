bd8fdbb5bf9ef75723f5cc5aa1c3a2076f330758
Artemiy <artoym13571@gmail.com>
2025-11-27 14:48:12 +0300

    Создан класс Snake, в main начата работа на окном

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import pygame
+import sys
+from pygame.color import THECOLORS
+
+
+
+screen = pygame.display.set_mode((1200, 800))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
---
+++ snake.py
@@ -0,0 +1,6 @@
+import pygame
+
+class Snake:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
