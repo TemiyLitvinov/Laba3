bc74344a851eba3c9f5b5573b9e19e5afa63a527
Artemiy <artoym13571@gmail.com>
2025-12-05 01:42:16 +0300

    Добавлен класс Food - еда и прописан базовый функционал

---
+++ food.py
@@ -0,0 +1,16 @@
+import random
+from snake import Snake
+
+
+class Food:
+    def __init__(self, snake : Snake):
+        self.pos = (0, 0)
+        self.respawn = snake
+
+    def random_respawn(self, snake : Snake):
+        x = random.randint(0, 30)
+        y = random.randint(0, 20)
+        if not snake.occupies((x, y)):
+            self.pos = (x, y)
+            return
+
